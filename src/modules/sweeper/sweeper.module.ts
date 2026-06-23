07e6c397c3374bf60923a8b8b69a5296f097f060
fighting45 <fighting45@gmail.com>
2026-06-23 16:23:21 +0500

    updated sweeper logic for fast finality

--- src/modules/sweeper/sweeper.module.ts
+++ src/modules/sweeper/sweeper.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SweepTransaction } from '../../entities';
+import { SweepTransaction, WalletAddress } from '../../entities';
 import { SweeperService } from './sweeper.service';
 import { SweeperController } from './sweeper.controller';
 import { WalletModule } from '../wallet/wallet.module';
 import { EncryptionModule } from '../encryption/encryption.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SweepTransaction]), WalletModule, EncryptionModule],
+  imports: [TypeOrmModule.forFeature([SweepTransaction, WalletAddress]), WalletModule, EncryptionModule],
   providers: [SweeperService],
   controllers: [SweeperController],
 })
